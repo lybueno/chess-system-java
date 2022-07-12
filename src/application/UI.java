c1002e3a474f7700e173340b4548cfdad0959d1d
Lygia Bueno <lybueno@gmail.com>
2022-07-12 11:20:51 -0300

    Possible moves of a piece

--- src/application/UI.java
+++ src/application/UI.java
@@ -31,6 +31,7 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 
     // https://stackoverflow.com/questions/2979383/java-claer-the-console
+    // it doesn't work for IntelliJ
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
